0da9432faa389ee173cb6979e34e9c760f1e2605
samuelbernardolip <44866834+samuelbernardolip@users.noreply.github.com>
2026-06-11 14:12:41 +0100

    Add JePL file structure

---
+++ .sqa/script.ginger_visionary_dhole.sh
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: Copyright contributors to the Software Quality Assurance as a Service (SQAaaS) project <sqaaas@ibergrid.eu>
+# SPDX-FileContributor: Pablo Orviz <orviz@ifca.unican.es>
+#
+# SPDX-License-Identifier: GPL-3.0-only
+
+(
+cd github.com/n8n-io/n8n &&
+    hadolint .devcontainer/Dockerfile docker/images/engine/Dockerfile docker/images/n8n-base/Dockerfile docker/images/n8n/Dockerfile docker/images/runners/Dockerfile packages/@n8n/benchmark/Dockerfile packages/testing/containers/dockerfiles/kent/Dockerfile --failure-threshold error
+)
--- .sqa/script.lanky-goldenrod-penguin.sh
+++
@@ -1,9 +0,0 @@
-# SPDX-FileCopyrightText: Copyright contributors to the Software Quality Assurance as a Service (SQAaaS) project <sqaaas@ibergrid.eu>
-# SPDX-FileContributor: Pablo Orviz <orviz@ifca.unican.es>
-#
-# SPDX-License-Identifier: GPL-3.0-only
-
-(
-cd github.com/n8n-io/n8n &&
-    hadolint .devcontainer/Dockerfile docker/images/n8n-base/Dockerfile docker/images/n8n/Dockerfile docker/images/runners/Dockerfile packages/@n8n/benchmark/Dockerfile packages/testing/containers/dockerfiles/kent/Dockerfile --failure-threshold error
-)
